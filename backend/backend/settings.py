d8760a319b5d8f37a977a0815e7e8d1e9a406119
VaishnaviRai287 <vaishnavi.rai287@gmail.com>
2026-07-24 03:50:56 +0530

    fix deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,10 +106,10 @@ USE_TZ = True
 # ─── Static Files ─────────────────────────────────────────────────────────
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # ─── Channel Layers ────────────────────────────────────────────────────────
